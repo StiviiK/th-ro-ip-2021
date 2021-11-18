08d5759b34dd3a38135ca93d6c8d6d5fe7f913fd
Stefan Kürzeder <info@stivik.me>
2021-11-18 21:30:43 +0100

    Cleanup azure terraform

--- infrastructure/azure/main.tf
+++ infrastructure/azure/main.tf
@@ -14,14 +14,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "2.80.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.3"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.4.1"
-    }
   }
 }
 
@@ -31,24 +23,6 @@ provider "azuread" {}
 
 provider "azurerm" {
   features {}
-}
-
-provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.default.kube_config.0.host
-
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host = azurerm_kubernetes_cluster.default.kube_config.0.host
-
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
-  }
 }
 
 data "azurerm_client_config" "current" {}
