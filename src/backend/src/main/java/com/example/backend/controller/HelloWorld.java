b8f7508e1bc994142078007e11788b23a055dbeb
Stefan Kürzeder <info@stivik.me>
2021-11-23 14:19:30 +0100

    Fixed CORS

--- src/backend/src/main/java/com/example/backend/controller/HelloWorld.java
+++ src/backend/src/main/java/com/example/backend/controller/HelloWorld.java
@@ -1,11 +1,13 @@
 package com.example.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-    @GetMapping(value = "/hello", produces = {"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
     public String hello() {
         return "Hello World";
     }
