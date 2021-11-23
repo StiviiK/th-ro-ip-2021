4684d5ea4b39325417a6b25c6216e05bdadc8add
Stefan Kürzeder <info@stivik.me>
2021-11-23 14:01:51 +0100

    Addendum to last commit

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService, User } from './services/authentication-service.service';
 
 @Component({
   selector: 'app-root',
