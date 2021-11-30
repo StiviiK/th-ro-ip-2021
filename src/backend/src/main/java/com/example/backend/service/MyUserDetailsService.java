96ac5ba78b90ef41ffe507e5878e935c0db36aa7
Stefan Kürzeder <info@stivik.me>
2021-11-30 13:05:52 +0100

    Load user from backend

--- src/backend/src/main/java/com/example/backend/service/MyUserDetailsService.java
+++ src/backend/src/main/java/com/example/backend/service/MyUserDetailsService.java
@@ -1,18 +1,69 @@
 package com.example.backend.service;
 
-import org.springframework.security.core.userdetails.User;
+import com.example.backend.models.User;
+import com.example.backend.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Todo: Temp
-        return new User("foo", "foo", new ArrayList<>());
+        var user = this.userRepository.getByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserPrincipal(user);
+    }
+}
+
+record UserPrincipal(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
