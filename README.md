a08a92af3f63b8214537ab43abef6611ab840679
Stefan Kürzeder <info@stivik.me>
2021-12-30 14:24:11 +0100

    Read Keywordservices url from Env

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Internet Programmierung 2021
 
 ## KeywordService Dev Setup
-``` bash
+
+```bash
 cd ./src/keyword_service/
 docker image build . -t th-ro-ip-keyword-service:latest
-docker run --name keyword-service -d -p 5002:5002 th-ro-ip-keyword-service:latest
+docker run --name keyword-service -d -p 8000:8000 th-ro-ip-keyword-service:latest
+```
+
+Set Environment Variable `KEYWORD_SERVICE_URL` to `http://localhost:8000` before running the server.
--- src/backend/src/main/java/com/example/backend/util/KeywordsApi.java
+++ src/backend/src/main/java/com/example/backend/util/KeywordsApi.java
@@ -10,17 +10,15 @@ import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class KeywordsApi {
-    private static String BaseURL = "http://localhost:5000/extract?text=";
+    private static final String BaseURL = System.getenv("KEYWORD_SERVICE_URL");
 
     public static JSONObject getKeywords(String text) throws URISyntaxException, IOException, InterruptedException {
-        String url = new StringBuilder()
-                .append(BaseURL)
-                .append(URLEncoder.encode(text, "UTF-8"))
-                .toString();
+        String url = String.format("%s/extract?text=%s", BaseURL, URLEncoder.encode(text, StandardCharsets.UTF_8));
         HttpRequest request = HttpRequest
                 .newBuilder(new URI(url))
                 .GET()
