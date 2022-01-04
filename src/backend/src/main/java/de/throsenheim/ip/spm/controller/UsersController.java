f3cfaa12ea0949bfa41c167effc0301effffff64
Stefan Kürzeder <info@stivik.me>
2022-01-04 16:13:21 +0100

    Addendum to last commit

--- src/backend/src/main/java/de/throsenheim/ip/spm/controller/UsersController.java
+++ src/backend/src/main/java/de/throsenheim/ip/spm/controller/UsersController.java
@@ -5,9 +5,10 @@ import de.throsenheim.ip.spm.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users", produces = {"application/json;charset=UTF-8"})
