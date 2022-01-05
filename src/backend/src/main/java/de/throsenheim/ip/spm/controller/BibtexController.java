66944525b64efe61bc1571536c01bf607ea3053d
sINFlumetz <lukas.metzner@stud.th-rosenheim.de>
2022-01-05 13:48:45 +0100

    Controller docstrings

--- src/backend/src/main/java/de/throsenheim/ip/spm/controller/BibtexController.java
+++ src/backend/src/main/java/de/throsenheim/ip/spm/controller/BibtexController.java
@@ -11,10 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+/**
+ * Controller that acts as an interface to arxiv.org for the Bibtex api.
+ *
+ * @author Lukas Metzner
+ */
 @RestController
 @RequestMapping(value = "/bibtex", produces = {"application/json;charset=UTF-8"})
 public class BibtexController {
 
+    /**
+     * Retrieve the Bibtex string for a single paper.
+     * @param id Arxiv.org id for the paper.
+     * @return Response that contains the Bibtex string.
+     * @throws Exception
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<BibtexResponse> bibtex(@PathVariable("id") String id) throws Exception {
         try {
