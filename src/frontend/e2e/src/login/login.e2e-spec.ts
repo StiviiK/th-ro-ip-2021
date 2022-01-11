512df6125c230e8a404dfee5cfe59610abfde183
sINFlumetz <lukas.metzner@stud.th-rosenheim.de>
2022-01-11 18:17:01 +0100

    Added frontend test docstrings.

--- src/frontend/e2e/src/login/login.e2e-spec.ts
+++ src/frontend/e2e/src/login/login.e2e-spec.ts
@@ -1,19 +1,33 @@
 import { AppPage } from '../app.po';
 import { browser, logging } from 'protractor';
 
+/**
+ * Test if login dialog works.
+ * 
+ * @author Lukas Metzner
+ */
 describe('Testing Login', () => {
   let page: AppPage;
 
+  /**
+   * Create new instance of the page.
+   */
   beforeEach(() => {
     page = new AppPage();
   });
 
+  /**
+   * Navigate to the page and perform the login.
+   */
   it('Should login.', async () => {
     await page.navigateTo();
     await page.performLogin();
     expect(await page.getMyPapersButton()).toEqual('My Papers');
   });
 
+  /**
+   * After tests -> logout.
+   */
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     await page.performLogout();
