947fddd6c425184eb7112b4ece4760c8927bd776
Stefan Kürzeder <info@stivik.me>
2022-01-13 13:57:49 +0100

    Fixed sonarqube issues

--- src/backend/src/main/java/de/throsenheim/ip/spm/configuration/LocalWebMvcConfigurer.java
+++ src/backend/src/main/java/de/throsenheim/ip/spm/configuration/LocalWebMvcConfigurer.java
@@ -14,7 +14,7 @@ public class LocalWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://ip.mirix.io", "http://localhost:4200", "http://127.0.0.1:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowedHeaders("*");
     }
